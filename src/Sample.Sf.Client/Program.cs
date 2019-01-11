c30517d8c08c43b4e0f34286bb1be26ea946f09e
Kam Lagan <kam.lagan@outlook.com>
2019-01-11 23:31:29 +0000

    changed to x64

--- src/Sample.Sf.Client/Program.cs
+++ src/Sample.Sf.Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.ServiceFabric.Services.Remoting.Client;
 using Sample.Sf.Common;
@@ -17,21 +19,37 @@ namespace Sample.Sf.Client
             Console.WriteLine("Press [RETURN] when ready to run test");
             Console.ReadLine();
 
+            await FabricServiceCall();
+
+            await WebApiCall();
+
+            Console.ReadLine();
+        }
+
+        static async Task WebApiCall()
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            
+            var callTask = client.GetStringAsync("http://vmkamlagan:8209/api/dummy");
+
+            var response = await callTask;
+
+            Console.WriteLine($"WebApi Call Result: {response}");
+        }
+
+        static async Task FabricServiceCall()
+        {
             var address = new FabricServiceAddress("fabric:/Sample.Sf.Application/Sample.Sf.DummyService");
 
             var client = ServiceProxy.Create<IMyService>(address);
 
             var result = await client.HelloWorldAsync();
 
-            Console.WriteLine($"Result: {result}");
-            Console.ReadLine();
-        }
-
-        static void WebApiCall()
-        {
-            //fabric:/Sample.Sf.Application/Sample.Sf.DummyService.WebApi
-
-
+            Console.WriteLine($"Fabric Service Call Result: {result}");
         }
     }
 }
